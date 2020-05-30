c7affe278a1c672c23dabf28428f69e306e820d0
Leroy <leroyjvargis@gmail.com>
2020-05-30 14:43:15 -0700

    generate component modules

--- projects/hr-app/src/app/app-routing.module.ts
+++ projects/hr-app/src/app/app-routing.module.ts
@@ -2,7 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'dashboard', 
+    loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) 
+  },
+  { 
+    path: 'people', 
+    loadChildren: () => import('./features/people/people.module').then(m => m.PeopleModule) 
+  }, 
+  { 
+    path: 'payroll', 
+    loadChildren: () => import('./features/payroll/payroll.module').then(m => m.PayrollModule) 
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

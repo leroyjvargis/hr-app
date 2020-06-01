a5d573476bec251208d5d9f055ae2f9e4d0f41cf
Leroy <leroyjvargis@gmail.com>
2020-06-01 11:51:04 -0700

    add mock table: list

--- projects/hr-app/src/app/features/payroll/payroll.component.ts
+++ projects/hr-app/src/app/features/payroll/payroll.component.ts
@@ -23,14 +23,34 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
 
+export interface PayrollData {
+  username: string;
+  payperiod: string;
+  type: string;
+  pay_rate: number;
+  hours_worked: number;
+  overtime_rate: number;
+  overtime_hours: number;
+  deductions: number;
+  tax_rate: number;
+  gross_pay: number
+}
+
+const PAY_DATA: PayrollData[] = [
+  {username: "Leroy Jacob Vargis", payperiod: "20-06", type: "hourly", pay_rate: 20, hours_worked: 40, overtime_rate: 1.5, overtime_hours: 8, deductions: 50, tax_rate: 15, gross_pay: 400},
+  {username: "John Doe", payperiod: "20-06", type: "salary", pay_rate: 80000, hours_worked: 40, overtime_rate: 2.5, overtime_hours: 2, deductions: 250, tax_rate: 25, gross_pay: 3400},
+  {username: "Jane Smith", payperiod: "20-06", type: "hourly", pay_rate: 20, hours_worked: 80, overtime_rate: 2, overtime_hours: 8, deductions: 30, tax_rate: 12, gross_pay: 800},
+  {username: "Anon Person", payperiod: "20-06", type: "salary", pay_rate: 60000, hours_worked: 40, overtime_rate: 2, overtime_hours: 14, deductions: 150, tax_rate: 20, gross_pay: 1400}
+]
+
 @Component({
   selector: 'app-payroll',
   templateUrl: './payroll.component.html',
   styleUrls: ['./payroll.component.scss']
 })
 export class PayrollComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['username', 'type', 'pay_rate', 'hours_worked', 'overtime_rate', 'overtime_hours', 'deductions', 'tax_rate', 'gross_pay', 'actions'];
+  dataSource = new MatTableDataSource<PayrollData>(PAY_DATA);
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
--- projects/hr-app/src/app/features/payroll/payroll.module.ts
+++ projects/hr-app/src/app/features/payroll/payroll.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { PayrollRoutingModule } from './payroll-routing.module';
 import { PayrollComponent } from './payroll.component';
@@ -16,6 +18,8 @@ import { PayrollComponent } from './payroll.component';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
+    MatIconModule,
+    MatButtonModule,
   ]
 })
 export class PayrollModule { }

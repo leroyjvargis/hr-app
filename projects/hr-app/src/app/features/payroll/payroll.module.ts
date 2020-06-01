e8852c35885261a4a9cf891e837485f77bf22caf
Leroy <leroyjvargis@gmail.com>
2020-05-31 21:06:29 -0700

    add mock table with pagination & sort

--- projects/hr-app/src/app/features/payroll/payroll.module.ts
+++ projects/hr-app/src/app/features/payroll/payroll.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 import { PayrollRoutingModule } from './payroll-routing.module';
 import { PayrollComponent } from './payroll.component';
 
@@ -7,7 +11,11 @@ import { PayrollComponent } from './payroll.component';
 @NgModule({
   declarations: [PayrollComponent],
   imports: [
-    PayrollRoutingModule
+    PayrollRoutingModule,
+
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
   ]
 })
 export class PayrollModule { }

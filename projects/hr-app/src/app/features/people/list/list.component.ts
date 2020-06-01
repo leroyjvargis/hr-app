e51188bfe6d21517741b7114c9d93cfac492b055
Leroy <leroyjvargis@gmail.com>
2020-05-31 21:26:44 -0700

    add phone & email to list

--- projects/hr-app/src/app/features/people/list/list.component.ts
+++ projects/hr-app/src/app/features/people/list/list.component.ts
@@ -12,14 +12,14 @@ export class ListComponent implements OnInit {
 
   ngOnInit(): void {
     this.mockData = [
-      { firstname: 'Leroy', lastname: 'Vargis', position: 'Software Developer', department: 'Technology', start_data: '2016-06-01' },
-      { firstname: 'Jacob', lastname: 'Vargis', position: 'Software Developer II', department: 'eCommerce', start_data: '2018-08-15' },
-      { firstname: 'John', lastname: 'Doe', position: 'HR Success Specialist', department: 'Human Resources', start_data: '2017-01-10' },
-      { firstname: 'Jane', lastname: 'Smith', position: 'Sales Engineer', department: 'Sales & Marketing', start_data: '2018-06-15' },
-      { firstname: 'Anonymous', lastname: 'Person', position: 'Manager, Sales', department: 'Sales & Marketing', start_data: '2019-07-14' },
-      { firstname: 'John', lastname: 'Doe', position: 'HR Success Specialist', department: 'Human Resources', start_data: '2017-01-10' },
-      { firstname: 'Jane', lastname: 'Smith', position: 'Sales Engineer', department: 'Sales & Marketing', start_data: '2018-06-15' },
-      { firstname: 'Jacob', lastname: 'Vargis', position: 'Software Developer II', department: 'eCommerce', start_data: '2018-08-15' },
+      { firstname: 'Leroy', lastname: 'Vargis', phone: '4801235544', email: 'leroy@hrapp.com', position: 'Software Developer', department: 'Technology', start_data: '2016-06-01' },
+      { firstname: 'Jacob', lastname: 'Vargis', phone: '4801235544', email: 'jacob@hrapp.com', position: 'Software Developer II', department: 'eCommerce', start_data: '2018-08-15' },
+      { firstname: 'John', lastname: 'Doe', phone: '4801235544', email: 'john@hrapp.com', position: 'HR Success Specialist', department: 'Human Resources', start_data: '2017-01-10' },
+      { firstname: 'Jane', lastname: 'Smith', phone: '4801235544', email: 'jane@hrapp.com', position: 'Sales Engineer', department: 'Sales & Marketing', start_data: '2018-06-15' },
+      { firstname: 'Anonymous', lastname: 'Person', phone: '4801235544', email: 'anon@hrapp.com', position: 'Manager, Sales', department: 'Sales & Marketing', start_data: '2019-07-14' },
+      { firstname: 'John', lastname: 'Doe', phone: '4801235544', email: 'doe@hrapp.com', position: 'HR Success Specialist', department: 'Human Resources', start_data: '2017-01-10' },
+      { firstname: 'Jane', lastname: 'Smith', phone: '4801235544', email: 'smith@hrapp.com', position: 'Sales Engineer', department: 'Sales & Marketing', start_data: '2018-06-15' },
+      { firstname: 'Jacob', lastname: 'Vargis', phone: '4801235544', email: 'vargis@hrapp.com', position: 'Software Developer II', department: 'eCommerce', start_data: '2018-08-15' },
     ]
   }
 

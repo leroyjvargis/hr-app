f5745d5cabaa3ee0b81ff776404f3b73580c022d
Leroy <leroyjvargis@gmail.com>
2020-06-01 15:45:30 -0700

    add updatepayroll

--- projects/hr-app/src/app/features/payroll/payroll.service.ts
+++ projects/hr-app/src/app/features/payroll/payroll.service.ts
@@ -3,9 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 export interface PayrollData {
+  id: string;
   username: string;
   payperiod: string;
-  type: string;
+  salary_type: string;
   pay_rate: number;
   hours_worked: number;
   overtime_rate: number;
@@ -37,5 +38,16 @@ export class PayrollService {
       }),
     };
     return this.httpClient.get<PayrollData[]>(url, httpOptions);
+  }
+
+  update(data: PayrollData) {
+    const url = "https://w73kurtrje.execute-api.us-east-1.amazonaws.com/beta-api/payroll";
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+      }),
+    };
+    return this.httpClient.put<PayrollData[]>(url, data, httpOptions);
+
   }
 }
